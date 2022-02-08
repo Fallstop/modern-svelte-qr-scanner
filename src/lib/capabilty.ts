619fea47f774929a7cf4d3e0a203c207f7b6e9bd
Jasper Miller-Waugh <contact@jmw.nz>
2022-02-08 21:05:02 +1300

    Removed WASM capability test

--- src/lib/capabilty.ts
+++ src/lib/capabilty.ts
@@ -14,23 +14,6 @@ export async function testCapabilities(): Promise<Error> {
             return new Error("Camera Permission denied")
         }
     } catch(e) {}
-
-    // From https://stackoverflow.com/a/47880734
-    const WasmSupported = (() => {
-        try {
-            if (typeof WebAssembly === "object"
-                && typeof WebAssembly.instantiate === "function") {
-                const module = new WebAssembly.Module(Uint8Array.of(0x0, 0x61, 0x73, 0x6d, 0x01, 0x00, 0x00, 0x00));
-                if (module instanceof WebAssembly.Module)
-                    return new WebAssembly.Instance(module) instanceof WebAssembly.Instance;
-            }
-        } catch (e) {
-        }
-        return false;
-    })();
-    if (!WasmSupported) {
-        return new Error("WebAssembly Not Supported")
-    }
     
     return null
 }
